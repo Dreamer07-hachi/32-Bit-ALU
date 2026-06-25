c8e7e3110ce3bdc97db6df6d8564a355da54774e
Rohan Pankajkumar Tatar <13rohantatar@gmail.com>
2026-06-25 15:01:57 +0530

    Update register.v

--- register.v
+++ register.v
@@ -1,14 +1,18 @@
 module Register(
-	input [31:0] load,
-	input clk,
-	input rst,
-	output reg [31:0] op
+    input wire [31:0] load,
+    input wire load_en,
+    input wire clk,
+    input wire rst,
+    output reg [31:0] op
 );
 
-always @(posedge clk or posedge rst ) begin
-	if(rst)
-		op = 32'h0000000;
-	else
-		op = load;
-end
+    // Triggers instantly on either the clock edge OR the reset edge
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            op <= 32'h00000000;  // Reset happens immediately
+        end else if (load_en) begin
+            op <= load;          // Capture data only if load_en is high
+        end
+    end
+
 endmodule
